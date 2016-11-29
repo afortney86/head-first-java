ce02672a5dbd2568e86ca5b66e51e71bf8b58efb
Anthony Fortney <afortney86@gmail.com>
2016-11-29 17:34:02 -0600

    Made dog oject

--- src/Dog.java
+++ src/Dog.java
@@ -2,4 +2,11 @@
  * Created by anthonyfortney on 11/29/16.
  */
 public class Dog {
+    int size;
+    String breed;
+    String name;
+
+    void bark() {
+        System.out.println("Ruff, Ruff");
+    }
 }
