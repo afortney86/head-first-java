2fe762d7f0c1b9887a81f87c97bf298e0615f2e6
Anthony Fortney <afortney86@gmail.com>
2016-11-29 17:34:18 -0600

    tested dog object

--- src/DogTestDrive.java
+++ src/DogTestDrive.java
@@ -2,4 +2,11 @@
  * Created by anthonyfortney on 11/29/16.
  */
 public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        d.size = 40;
+
+        d.bark();
+    }
 }
