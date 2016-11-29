8591d5093ea660b254aed8ffdf6e2ad18794f721
Anthony Fortney <afortney86@gmail.com>
2016-11-29 17:44:07 -0600

    Movie object

---
+++ src/Movies.java
@@ -0,0 +1,12 @@
+/**
+ * Created by anthonyfortney on 11/29/16.
+ */
+public class Movies {
+    String title;
+    String genre;
+    int rating;
+
+    void playItOut(){
+        System.out.println("Playing the movie");
+    }
+}
