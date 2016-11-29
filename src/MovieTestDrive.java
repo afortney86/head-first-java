e27d8e5470d27a6fa02da57ba2982bd3b22c85ef
Anthony Fortney <afortney86@gmail.com>
2016-11-29 17:44:25 -0600

    Test Movie Object

---
+++ src/MovieTestDrive.java
@@ -0,0 +1,22 @@
+/**
+ * Created by anthonyfortney on 11/29/16.
+ */
+public class MovieTestDrive {
+    public static void main(String[] args) {
+        Movies one = new Movies();
+        one.title = "Gone with the stock";
+        one.genre = "Tragic";
+        one.rating = -2;
+
+        Movies two = new Movies();
+        two.title = "Lost in Cubicle Space";
+        two.genre = "Comedy";
+        two.rating = 5;
+        two.playItOut();
+
+        Movies three = new Movies();
+        three.title = "Byte Club";
+        three.genre = "Tragic but ultimately uplifting";
+        three.rating = 127;
+    }
+}
