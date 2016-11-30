a80314dcddd512978cb29326c13080a2d79c104e
Anthony Fortney <afortney86@gmail.com>
2016-11-29 18:40:36 -0600

    Guess game player guess method

--- src/GuessGamePlayer.java
+++ src/GuessGamePlayer.java
@@ -2,7 +2,9 @@
  * Created by anthonyfortney on 11/29/16.
  */
 public class GuessGamePlayer {
+
     int number = 0;
+
     public void guess() {
         number = (int) (Math.random() * 10);
         System.out.println("I'm guessing " + number);
