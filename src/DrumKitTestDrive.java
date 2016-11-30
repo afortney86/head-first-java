bc64022ce4c4529ec9defce2bbf72d7eb1a85685
Anthony Fortney <afortney86@gmail.com>
2016-11-29 19:17:51 -0600

    DrumKitTD played drums

--- src/DrumKitTestDrive.java
+++ src/DrumKitTestDrive.java
@@ -2,5 +2,15 @@
  * Created by anthonyfortney on 11/29/16.
  */
 public class DrumKitTestDrive {
+    public static void main(String[] args) {
 
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.snare = false;
+        d.playTopHat();
+
+        if (d.snare == true) {
+            d.playSnare();
+        }
+    }
 }
