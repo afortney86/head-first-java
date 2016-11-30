b71872818150ec17fac1f9f486be86dc330ca1cd
Anthony Fortney <afortney86@gmail.com>
2016-11-29 19:13:34 -0600

    created drumKit object

---
+++ src/DrumKit.java
@@ -0,0 +1,16 @@
+/**
+ * Created by anthonyfortney on 11/29/16.
+ */
+public class DrumKit {
+
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playTopHat(){
+        System.out.println("ding ding da-ding");
+    }
+
+    void playSnare(){
+        System.out.println("bang bang ba-bang");
+    }
+}
