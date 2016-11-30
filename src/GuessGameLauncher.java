c1bc9c26879c817f16ccad00880b16d091bfd6c6
Anthony Fortney <afortney86@gmail.com>
2016-11-29 18:39:51 -0600

    Guess game launcher

--- src/GuessGameLauncher.java
+++ src/GuessGameLauncher.java
@@ -2,4 +2,8 @@
  * Created by anthonyfortney on 11/29/16.
  */
 public class GuessGameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
 }
